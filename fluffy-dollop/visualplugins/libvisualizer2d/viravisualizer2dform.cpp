5b4d86df72fa1fe278505a8baf467ea094b1e9d0
serg9458989 <serg9458989@mail.ru>
2017-05-05 00:25:20 +0400

    управление шириной таскалки сплиттера

--- fluffy-dollop/visualplugins/libvisualizer2d/viravisualizer2dform.cpp
+++ fluffy-dollop/visualplugins/libvisualizer2d/viravisualizer2dform.cpp
@@ -1,11 +1,23 @@
 #include "viravisualizer2dform.h"
 #include "ui_viravisualizer2dform.h"
+#include <ctrcore/ctrcore/ctrconfig.h>
 
 ViraVisualizer2dForm::ViraVisualizer2dForm(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ViraVisualizer2dForm)
 {
     ui->setupUi(this);
+
+    int handleWidth(10);
+    QVariant handleWidthVar = CtrConfig::getValueByName("application_settings.mainSplittetHandleWidth_Pixels", 10, true);
+    if(handleWidthVar.isValid())
+    {
+        if(handleWidthVar.toUInt() > 0 && handleWidthVar.toUInt() <= 20)
+            handleWidth = handleWidthVar.toUInt();
+        else
+            CtrConfig::setValueByName("application_settings.mainSplittetHandleWidth_Pixels", 10);
+    }
+    ui->splitter->setHandleWidth(handleWidth);
 }
 
 ViraVisualizer2dForm::~ViraVisualizer2dForm()
